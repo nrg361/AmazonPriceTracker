2e65e2dbe19d1f34ff6c70100b1aaae000d49ae0
Anurag Shrivastava <nrgshrivastava361@gmail.com>
2021-06-18 18:52:30 +0530

    my first commit

--- parser.js
+++ parser.js
@@ -34,7 +34,7 @@ function go(tmp) {
 
 
     const transporter = nodemailer.createTransport({
-        service: 'hotmail',
+        service: 'gmail',
         auth: {
             user: email,
             pass: pswrd
@@ -47,8 +47,8 @@ function go(tmp) {
         subject: 'HURRYYYY!!! Price dropped',
         text: `The price of your product ${url} has dropped below ${myPrice}... Grab the deal before the sale ends!`
     };
-
-    const task = cron.schedule('*/10 * * * * *', () => compare(transporter, mailOptions, myPrice, url, task));
+    sendIt(transporter, mailOptions);
+    //const task = cron.schedule('*/10 * * * * *', () => compare(transporter, mailOptions, myPrice, url, task));
 }
 
 function sendIt(transporter, mailOptions) {
